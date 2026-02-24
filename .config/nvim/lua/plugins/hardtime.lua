ac5a9087ac0fb3b4fedebc3aa7c75e96dea90c94
PierreLouisPAUGAM <paugam.pl@icloud.com>
2026-02-24 13:07:59 +0100

    feat(nvim): add hardtime.nvim plugin for better vim motions

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,10 @@
+return {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    restriction_mode = "hint",
+    max_count = 4,
+    disable_mouse = false,
+  },
+}
